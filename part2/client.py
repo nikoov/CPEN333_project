9f00f3ba925273bbba73f763459ed420074d645d
SaraHematy <116848231+SaraHematy@users.noreply.github.com>
2024-12-05 20:44:13 -0800

    Update client.py

--- part2/client.py
+++ part2/client.py
@@ -3,16 +3,21 @@ import socket
 import threading
 
 class ChatClient:
+
     def __init__(self, window: Tk):
         self.window = window
         self.window.title("Client Chat")
 
         # Create a socket
         self.clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #Initializes client ID
+        self.client_id = None
 
         try:
             self.clientSocket.connect(('127.0.0.1', 12345))
             self.client_port = self.clientSocket.getsockname()[1]  # Get the client's port
+            #self.client_name = self.clientSocket.recv(1024).decode()  
+
             print(f"Connection Successful. Client is using port {self.client_port}")
         except socket.error as e:
             print(f"Connection failed: {e}")
@@ -46,11 +51,8 @@ class ChatClient:
     def send_message(self, event=None):
         new_message = self.client_entry.get()
         if new_message.strip():
-            self.chat_history.config(state="normal")
-            self.chat_history.insert(END, f"You: {new_message}\n")
-            self.chat_history.config(state="disabled")
-            self.chat_history.see(END)
             self.clientSocket.send(new_message.encode())
+            self.update_chat_history(f"{new_message}", align="right")
             self.client_entry.delete(0, END)
 
     def receive_message(self):
@@ -59,15 +61,17 @@ class ChatClient:
                 server_message = self.clientSocket.recv(1024).decode()
                 if server_message:
                     self.window.after(0, self.update_chat_history, server_message)
-            except (ConnectionResetError, BrokenPipeError):
-                print("Server disconnected.")
+            except (ConnectionResetError, BrokenPipeError): #check if code is necessary
                 break
 
-    def update_chat_history(self, message):
+    def update_chat_history(self, message, align="left"):
         self.chat_history.config(state="normal")
-        self.chat_history.insert(END, f"Server: {message}\n")
-        self.chat_history.config(state="disabled")
-        self.chat_history.see(END)
+        if align == "left":
+            self.chat_history.insert(END, f"{message.rjust(25)}\n", "right")
+        else:
+            self.chat_history.insert(END, f"{message}\n", "left")
+            self.chat_history.config(state="disabled")
+            self.chat_history.see(END)
 
 def main():
     window = Tk()
@@ -76,4 +80,6 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+    
 
